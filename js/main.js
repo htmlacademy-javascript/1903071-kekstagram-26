be2ddc1b0e46aa1432b5ba18cc2bfc3659564505
Nikolay Teplyashin <lukeimyourfazer@gmail.com>
2022-06-20 23:05:24 +0300

    Попытался сделать дз)

--- js/main.js
+++ js/main.js
@@ -1,11 +1,52 @@
-// Взято с https://learn.javascript.ru/
+const messagesInComments = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
 
-const randomNumber = function (min, max) {
+const namesInComments = [
+  'Артем',
+  'Ольга',
+  'Путин Х.',
+  'Алексей',
+  'Елизавета',
+  'Иосиф',
+  'Адольф'
+];
+
+
+const photosDescriptionsCount = 25;
+
+const getRandomNumber = function (min, max) {
   const random = min - 0.5 + Math.random() * (max - min + 1);
   return Math.round(random);
 };
 
-randomNumber();
+const getRandomArrayElement = (elements) =>
+  elements[getRandomNumber(0, elements.length - 1)];
+
+const createComments = () => ({
+  id: getRandomNumber(1, 999),
+  avatar: `img/avatar-${getRandomNumber(1, 6)}.svg`,
+  message: getRandomArrayElement(messagesInComments),
+  name: getRandomArrayElement(namesInComments)
+});
+
+const randomComments = Array.from({length: 2}, createComments);
+
+
+const createPhotoDescription = () => ({
+  id: getRandomNumber(1, 25),
+  url: `photos/${getRandomNumber(1, 25)}.jpg`,
+  description: 'Описание фотографии',
+  likes: getRandomNumber(15, 200),
+  comments: randomComments
+});
+
+const photosDescriptions = Array.from({length: photosDescriptionsCount}, createPhotoDescription);
 
 
 // Написал сам
@@ -17,6 +58,5 @@ const checkCommentLength = function (line, maxLength) {
     return false;
   }
 };
-
-checkCommentLength();
+checkCommentLength('asdsad', 5);
 
